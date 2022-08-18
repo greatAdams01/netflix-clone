f380160185ff9501045c48e6c0d6e016c651f6ce
greatAdams01 <kingifean@gmail.com>
2022-08-18 10:31:59 +0100

    project init

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>NetFlex</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+      </main>
+    </div>
+  )
+}
+
+export default Home
